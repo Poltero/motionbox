2aa64916fdf18b2343fd0861bf4f50efd3d7a387
Pablo Fernandez <poltero@ovi.com>
2012-11-09 00:03:02 +0100

    File configuration of application

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+/**
+File configuration application
+ * @author: Pablo Fernandez Franco
+ * @author web: http://www.pablo-franco.com
+ * Please do not remove the credits
+*/
+
+//Config Silex
+
+//Enable debug
+$app['debug'] = true;
+
+//Register services
+//Monolog
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/log/development.log',
+));
+
+//Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/web',
+));
+
+//Doctrine
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'    => 'pdo_mysql',
+        'path'      => __DIR__.'/app.db',
+        'host'      => 'mysql_read.someplace.tld',
+        'dbname'    => 'my_database',
+        'user'      => 'my_username',
+        'password'  => 'my_password'
+    ),
+));
+
