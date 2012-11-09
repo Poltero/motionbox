fc5356d318bc50b6efdbfc299c2910674f77a74a
Pablo Fernandez <poltero@ovi.com>
2012-11-09 11:04:23 +0100

    use Response object

--- src/controllers.php
+++ src/controllers.php
@@ -6,10 +6,12 @@ Controllers of app
  * Please do not remove the credits
 */
 
+use Symfony\Component\HttpFoundation\Response;
+
 $app->get('/', function(){
-    return 'Hola';
+    return new Response('Hello world');
 });
 
 $app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+    return 'Hello '.$app->escape($name);
 });
