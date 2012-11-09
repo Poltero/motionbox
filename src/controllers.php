45d9d4aee9acffdd82b3476493b1ff7147a2ac7d
Pablo Fernandez <poltero@ovi.com>
2012-11-09 11:00:55 +0100

    Add new rules gitignore

--- src/controllers.php
+++ src/controllers.php
@@ -6,6 +6,10 @@ Controllers of app
  * Please do not remove the credits
 */
 
+$app->get('/', function(){
+    return 'Hola';
+});
+
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
 });
